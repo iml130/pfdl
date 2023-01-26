30d1c766c23bde9102082e0d09930212173cb037
Maximilian Hoerstrup <maximilian.hoerstrup@iml.fraunhofer.de>
2023-01-26 14:59:39 +0100

    Add an input parameter copy for substitution
    To substitute the counting variables in the input parameters, a copy
    of the input parameters from service or service calls have to be made

--- pfdl_scheduler/api/service_api.py
+++ pfdl_scheduler/api/service_api.py
@@ -3,25 +3,24 @@
 # Licensed under the MIT License.
 # For details on the licensing terms, see the LICENSE file.
 # SPDX-License-Identifier: MIT
-
 """Contains the ServiceAPI class."""
-
 # standard libraries
+import copy
+from typing import List, Union
 from uuid import uuid4
 from dataclasses import dataclass
 
 # local sources
 from pfdl_scheduler.api.task_api import TaskAPI
 from pfdl_scheduler.model.service import Service
+from pfdl_scheduler.model.struct import Struct
 
 
 @dataclass
 class ServiceAPI:
     """Represents a called Service.
-
     Represents a Service or Service Call in the langauge which can be mapped to a real
     service that can be executed.
-
     Attributes:
         service: A description of the called Service.
         task_context: A TaskAPI representaiton of the Task from which the service was called.
@@ -37,7 +36,6 @@ class ServiceAPI:
         in_loop: bool = False,
     ) -> None:
         """Initialize the object.
-
         Args:
             service: A description of the called Service.
             task_context: A TaskAPI representaiton of the Task from which the service was called.
@@ -51,3 +49,6 @@ class ServiceAPI:
         self.in_loop: bool = in_loop
         self.service: Service = service
         self.task_context: TaskAPI = task_context
+        self.input_parameters: List[Union[str, List[str], Struct]] = copy.deepcopy(
+            service.input_parameters
+        )
