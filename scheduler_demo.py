e38a4eb6434efb6f609ee1620e88a8c69d6ab275
Maximilian Hoerstrup <maximilian.hoerstrup@iml.fraunhofer.de>
2023-02-15 17:58:21 +0100

    Add Petrinet and Log Observer to the demo
    -Two observers where added, so changes of the petri net or scheduling
    can be handled from outside.
    - A host can be specified (e.g. a dashboard), so the information can be
    send to a server (with an API)

--- scheduler_demo.py
+++ scheduler_demo.py
@@ -8,12 +8,87 @@
 
 # standard libraries
 import argparse
+from typing import Any
+import requests
+import base64
+import logging
+from datetime import datetime
 
 # local sources
 from pfdl_scheduler.api.service_api import ServiceAPI
+from pfdl_scheduler.api.observer_api import NotificationType, Observer
 from pfdl_scheduler.api.task_api import TaskAPI
 from pfdl_scheduler.model.struct import Struct
 from pfdl_scheduler.scheduler import Scheduler, Event
+
+# do not log requests entries if it is lesser than a warning
+logging.getLogger("requests").setLevel(logging.WARNING)
+logging.getLogger("urllib3").setLevel(logging.WARNING)
+
+
+class PetriNetObserver(Observer):
+    """PetriNetObserver for receiving infos about a change of the PetriNet.
+
+    The Observer will send a post request to the dashboard with the Petri Net image
+    encoded with base64.
+    """
+
+    def __init__(self, host: str):
+        self.host = host
+
+    def update(self, notification_type: NotificationType, data: Any) -> None:
+        if notification_type == NotificationType.PETRI_NET:
+            encoded_string = ""
+            with open("temp/petri_net.png", "rb") as file:
+                encoded_string = base64.b64encode(file.read())
+
+            request_data = {
+                "order_id": "8bf4eb6a-74df-427c-a475-532392465f70",
+                "content": b"data:image/png;base64," + encoded_string,
+                "type_pn": "png",
+            }
+
+            requests.post(url=self.host + "/petri_net", data=request_data)
+
+
+class LogEntryObserver(Observer):
+    """LogEntryObserver for receiving logging information from the Scheduler.
+
+    LogLevels are based of https://docs.python.org/3/library/logging.html#logging-levels
+    """
+
+    def __init__(self, host: str = ""):
+        logging.basicConfig(
+            filename="temp/scheduler.log",
+            encoding="utf-8",
+            level=logging.DEBUG,
+            filemode="w",
+        )
+
+        if host == "":
+            self.dashboard_available: bool = False
+        else:
+            self.dashboard_available: bool = True
+            self.host = host
+
+    def update(self, notification_type: NotificationType, data: Any) -> None:
+        if notification_type == NotificationType.LOG_ENTRY:
+            log_entry = data[0]
+            log_level = data[1]
+            log_date = str(datetime.now())
+
+            if self.dashboard_available:
+                data = {
+                    "order_id": "8bf4eb6a-74df-427c-a475-532392465f70",
+                    "logDate": log_date,
+                    "logLevel": log_level,
+                    "logEntry": log_entry,
+                }
+
+                requests.post(url=self.host + "/log_entry", data=data)
+
+            log_entry = log_date + ": " + log_entry
+            logging.log(log_level, log_entry)
 
 
 class DemoInterface:
@@ -95,12 +170,25 @@ def main():
     parser = argparse.ArgumentParser(description="Process some integers.")
     parser.add_argument("file_path", type=str, help="the path for the PFDL file.")
     parser.add_argument(
+        "-dh", "--dashboard_host", type=str, help="the host address of the PFDL dashboard."
+    )
+    parser.add_argument(
         "--test_ids",
         action="store_true",
         help="services and tasks get test ids starting from 0.",
     )
     args = parser.parse_args()
     scheduler = Scheduler(args.file_path, args.test_ids)
+
+    dashboard_host = ""
+    if args.dashboard_host:
+        dashboard_observer = PetriNetObserver(args.dashboard_host)
+        scheduler.attach(dashboard_observer)
+        dashboard_host = args.dashboard_host
+
+    log_entry_observer = LogEntryObserver(dashboard_host)
+    scheduler.attach(log_entry_observer)
+
     demo_interface = DemoInterface(scheduler)
     demo_interface.start()
 
